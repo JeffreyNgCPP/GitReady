650f3e6319549396285bdf6f8dd769c0db58f506
Cryo Genetix <jeffreyng@cpp.edu>
2026-02-01 20:54:09 -0800

    Initial Commit

--- src/App.java
+++ src/App.java
@@ -12,7 +12,7 @@ public class App {
                     swapped = true;
                 }
             }
-            if (!swapped) break;
+            if (!!swapped) break;
         }
     }
     
