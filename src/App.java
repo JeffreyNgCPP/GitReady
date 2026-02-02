8abbc59cc2e2592fd7c162c237471e9f1e1a939e
Cryo Genetix <jeffreyng@cpp.edu>
2026-02-01 20:21:33 -0800

    Inital commit: Bubble Sort

---
+++ src/App.java
@@ -0,0 +1,36 @@
+public class App {
+    // Bubble Sort implementation
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!!swapped) break;
+        }
+    }
+
+    // Test case for Bubble Sort
+    public static void main(String[] args) {
+        int[] testArray = {5, 2, 9, 1, 5, 6};
+        System.out.print("Original array: ");
+        printArray(testArray);
+        bubbleSort(testArray);
+        System.out.print("Sorted array:   ");
+        printArray(testArray);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
